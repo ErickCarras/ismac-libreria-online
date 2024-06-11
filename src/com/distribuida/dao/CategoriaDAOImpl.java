5bf01aa8b8363e58fb3c5efa96368f4ed705c373
PC <PC@DESKTOP-OU6FR72.bbrouter>
2024-06-10 22:30:42 -0500

    Merge branch 'master' of https://github.com/ErickCarras/ismac-libreria-online.git

---
+++ src/com/distribuida/dao/CategoriaDAOImpl.java
@@ -0,0 +1,39 @@
+package com.distribuida.dao;
+
+import java.util.List;
+
+import com.distribuida.entities.Categoria;
+
+public class CategoriaDAOImpl implements CategoriaDAO {
+
+	@Override
+	public List<Categoria> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Categoria finOne(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void add(Categoria categoria) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void up(Categoria categoria) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void del(int id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
