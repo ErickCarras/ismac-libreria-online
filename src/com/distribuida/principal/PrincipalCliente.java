65b110189df93d47640d58cae00aa1025b7f0db4
ericc <ericc@DESKTOP-SNMI0CH.bbrouter>
2024-06-30 19:22:43 -0500

    v

--- src/com/distribuida/principal/PrincipalCliente.java
+++ src/com/distribuida/principal/PrincipalCliente.java
@@ -14,18 +14,25 @@ public class PrincipalCliente {
 		// TODO Auto-generated method stub
 		
 			ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
-			
 			ClienteDAO clienteDAO = context.getBean("clienteDAOImpl", ClienteDAO.class);
 			
-			List<Cliente> clientes = clienteDAO.findAll();
+			// CRUD
+			// add
+			Cliente cliente = new Cliente(0,"1723597854", "juan", "taipe", "av. por ahi y mas alla.", "0987452316", "jtaipe@correo.com");
+			clienteDAO.add(cliente);
 			
-			clientes.forEach(item -> {
-				
-				
-				System.out.println(item.toString());
-				
-				
-			});
+			// up
+			Cliente cliente2 = new Cliente(1, "1723597852", "juan2", "taipe2", "av. por ahi y mas alla.2", "0987452312", "jtaipe2@correo.com");
+			clienteDAO.up(cliente2);
+			
+			// del
+			clienteDAO.del(1);
+			
+			// findOne
+			System.out.println("************ DEL **************** "+clienteDAO.findOne(1));
+	
+			// find All
+		// clienteDAO.findAll().forEach(item -> { System.out.println(item.toString());  });
 		
 			context.close();
 		
